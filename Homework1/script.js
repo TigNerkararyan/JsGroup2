f44d62f61939259d8c638156a093d3731f646118
Hrach444 <hrachyamarkosyan4@gmail.com>
2021-12-17 19:49:15 +0400

    i

---
+++ Homework1/script.js
@@ -0,0 +1,12 @@
+let i = 1;
+
+while(i <= 50){
+    console.log(i);
+    i++;  
+};
+
+
+
+for(let a = 1; a <= 50; a++){
+    console.log(a);
+};
---
+++ Homework3/script.js
@@ -0,0 +1,9 @@
+let countries = {
+    Minsk: "Belarus",
+    Moscow: "Russia",
+    Kiev: "Ukraine"
+};
+
+for (let key in countries) {
+    console.log("Keys: " + key + " - " + "Values: " + countries[key]);
+};
