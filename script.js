b9e2ee8b2f1c458bc690d0c112027a9085e1e027
Sos-Tonoyan <sos.tonoyan.2021@mail.ru>
2021-12-21 21:10:31 +0400

    sortavorum

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("maladec bacec!");
+function array(arr){
+    let c;
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i] > arr[i+1]){
+                c = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = c
+            }
+            
+        }        
+    }
+    return arr
+}
+
+console.log(array([5,4,8,3,6,9]))
